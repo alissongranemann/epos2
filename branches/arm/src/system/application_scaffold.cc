1cfb43b60fa71c796785d68eaabf1cb3840d6c84
guto <guto@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2015-01-22 16:40:49 +0000

    Creating EPOS 2.0 ARM branch
    git-svn-id: https://epos.lisha.ufsc.br/svn/epos2@3352 86035c5f-d46c-4136-a2d5-fe63f1d4a782

---
+++ branches/arm/src/system/application_scaffold.cc
@@ -0,0 +1,17 @@
+// EPOS Application Scaffold and Application Abstraction Implementation
+
+#include <application.h>
+//#include <framework/main.h>
+
+// Application class attributes
+__BEGIN_SYS
+char Application::_preheap[];
+Heap * Application::_heap;
+//Framework::Cache Framework::_cache;
+__END_SYS
+
+// Global objects
+__BEGIN_API
+OStream cout;
+OStream cerr;
+__END_API
