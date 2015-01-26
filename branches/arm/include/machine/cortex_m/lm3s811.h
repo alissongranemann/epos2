6a7d28349d3ed1998d6290b6d40671ff948d55bb
davir <davir@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2015-01-26 19:50:39 +0000

    Merging emote3 code, with working UART and valid flash image generation.
    git-svn-id: https://epos.lisha.ufsc.br/svn/epos2@3358 86035c5f-d46c-4136-a2d5-fe63f1d4a782

--- branches/arm/include/machine/cortex_m/lm3s811.h
+++ branches/arm/include/machine/cortex_m/lm3s811.h
@@ -265,6 +265,41 @@ public:
         DEN_DIGP7     = 1 <<  7       // Pin 7 (1 -> Digital Enable)    r/w     1
     };
 
+    static void init_clock()
+    {
+        CPU::Reg32 rcc = scr(RCC);
+
+        // bypass PLL and system clock divider while initializing
+        rcc |= RCC_BYPASS;
+        rcc &= ~RCC_USESYSDIV;
+        scr(RCC) = rcc;
+
+        // select the crystal value and oscillator source
+        rcc &= ~RCC_XTAL_8192;
+        rcc |= RCC_XTAL_6000;
+        rcc |= RCC_MOSC;
+
+        // activate PLL by clearing PWRDN and OEN
+        rcc &= ~RCC_PWRDN;
+        rcc &= ~RCC_OEN;
+
+        // set the desired system divider and the USESYSDIV bit
+        rcc |= RCC_SYSDIV_4;
+        rcc |= RCC_USESYSDIV;
+        scr(RCC) = rcc;
+
+        // wait for the PLL to lock by polling PLLLRIS
+PLL_Init_loop:
+        CPU::Reg32 ris = scr(RIS);
+        ris &= RIS_PLLLRIS;
+        if(ris)
+            goto PLL_Init_loop;
+
+        // enable use of PLL by clearing BYPASS
+        rcc &= ~RCC_BYPASS;
+        scr(RCC) = rcc;
+    }
+
 protected:
     LM3S811() {}
 
