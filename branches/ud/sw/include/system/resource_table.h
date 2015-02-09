d2997d13805a55196d01c554a249fcbef9983fb2
jgreis <jgreis@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2015-02-09 20:52:56 +0000

    Deprecate Resource_Table.
    git-svn-id: https://epos.lisha.ufsc.br/svn/epos2@3389 86035c5f-d46c-4136-a2d5-fe63f1d4a782

--- branches/ud/sw/include/system/resource_table.h
+++
@@ -1,6 +0,0 @@
-#ifndef __resource_table_h
-#define __resource_table_h
-
-#include "../../../unified/framework/resource_table.h"
-
-#endif
--- branches/ud/sw/src/abstraction/resource_table.cc
+++
@@ -1,6 +0,0 @@
-
-#include <system/resource_table.h>
-
-#include "../../../unified/framework/resource_table_iids.h"
-
-
