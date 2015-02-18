823fac5b34b16966c93f5390ddf2262f25db1292
jgreis <jgreis@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2015-02-18 15:51:31 +0000

    Remove unused header file.
    git-svn-id: https://epos.lisha.ufsc.br/svn/epos2@3403 86035c5f-d46c-4136-a2d5-fe63f1d4a782

--- branches/ud/hw/system/resource_table.h
+++
@@ -1,7 +0,0 @@
-#ifndef __resource_table_h
-#define __resource_table_h
-
-#include "../../unified/framework/resource_table.h"
-#include "../../unified/framework/resource_table_iids.h"
-
-#endif /* __resource_h */
