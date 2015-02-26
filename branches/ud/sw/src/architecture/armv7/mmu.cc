961fdb60f6dd973d9d251137d99ecaa187e930e6
jgreis <jgreis@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2015-02-26 13:50:17 +0000

    Zynq's MMU implemtation must be refactored. Initiallly replace it with trunk's MMU.
    git-svn-id: https://epos.lisha.ufsc.br/svn/epos2@3420 86035c5f-d46c-4136-a2d5-fe63f1d4a782

--- branches/ud/sw/src/architecture/armv7/mmu.cc
+++ branches/ud/sw/src/architecture/armv7/mmu.cc
@@ -1,49 +1,10 @@
 // EPOS-- ARMv7 MMU Mediator Implementation
 
-#include <mmu.h>
-#include <machine.h>
-#include <system/traits.h>
+#include <architecture/armv7/mmu.h>
 
 __BEGIN_SYS
 
 // Class attributes
 ARMv7_MMU::List ARMv7_MMU::_free;
-/*
-ARMv7_MMU::Phy_Addr ARMv7_MMU::alloc(unsigned int bytes) {
-    Phy_Addr phy(false);
-    if(bytes) {
-        List::Element * e = _free.search_decrementing(bytes);
-        if(e){
-			//phy = e->object() + e->size();
-            phy = (unsigned int)e->object() + (unsigned int)e->size();
-			kout << "MMU::alloc: e->object()="<<(unsigned int)e->object()
-				<< ", e->size()="<<(unsigned int)e->size()<<endl;
-		}
-        else {
-            kout << "ARMv7_MMU::alloc() failed!\n";
-            db<ARMv7_MMU>(WRN) << "ARMv7_MMU::alloc() failed!\n";
-        }
-    }
-    db<ARMv7_MMU>(TRC) << "ARMv7_MMU::alloc(bytes=" << bytes << ") => "
-                      << (void *)phy << "\n";
-    //kout << "ARMv7_MMU::alloc(bytes=" << bytes << ") => " << (void *)phy << "\n";
-    return phy;
-}
-*/
-void ARMv7_MMU::free(Phy_Addr addr, int n) {
-    db<ARMv7_MMU>(TRC) << "ARMv7_MMU::free(addr=" << (void *)addr
-                      << ",n=" << n << ")\n";
 
-	if(addr % 4 != 0){
-		db<ARMv7_MMU>(ERR) << "Unaligned address to be freed!\n";
-		kout << "Unaligned address to be freed!\n";
-		Machine::panic();
-	}
-
-    if(addr && n) {
-       	List::Element * e = new (addr) List::Element(addr, n);
-        List::Element * m1, * m2;
-        _free.insert_merging(e, &m1, &m2);
-    }
-}
 __END_SYS
--- branches/ud/sw/src/architecture/armv7/mmu_init.cc
+++ branches/ud/sw/src/architecture/armv7/mmu_init.cc
@@ -1,142 +1,25 @@
-// EPOS ARMv7 MMU Mediator Initialization
+// EPOS-- ARMv7 MMU Mediator Initialization
 
 #include <mmu.h>
 #include <system.h>
 
-//extern "C" unsigned __bss_end__;
-//extern "C" unsigned MMUTable;
+extern "C" void * __data_start;
+extern "C" void * _edata;
+extern "C" void * __bss_start;
+extern "C" void * _end;
 
 __BEGIN_SYS
 
 void ARMv7_MMU::init()
 {
-    db<Init, ARMv7_MMU>(TRC) << "ARMv7_MMU::init()\n";
+    db<Init, ARMv7_MMU>(TRC) << "ARMv7_MMU::init()" << endl;
 
-    /* L1 PT: 4096 entries 4 bytes long.
-     * L2 PT: 256 entries 4 bytes long. There is one L2 PT for each entry in L1 (4096).
-     * Therefore, MMU page tables size = 4*4096 + 4096*4*256 = 0x404000
-     * However, the current implementation maps the 512mb:1023mb into 0:511mb, saving 512*1024 bytes.
-     * */
+    db<Init, ARMv7_MMU>(INF) << "ARMv7_MMU::init::dat.b=" << &__data_start << ",dat.e=" << &_edata << ",bss.b="
+                             << &__bss_start << ",bss.e=" << &_end << endl;
 
-    ARMv7_MMU::build_PTs(); //Build page tables.
-    ARMv7_MMU::enable_caches();
-
-    /*unsigned int* p = (unsigned int*)0x1ffffffc;
-    unsigned int* q = (unsigned int*)(0x1ffffffc + 0x20000000);
-    *p = 0xabcd;
-    kout << "*p=" << *p << endl;
-    kout << "*q=" << *q << endl;*/
-
-    // TODO: This is causing a prefetch abort
-    //db<Init, ARMv7_MMU>(TRC)<<"Enabling MMU...";
-    //ARMv7_MMU::enable();
-    //db<Init, ARMv7_MMU>(TRC)<<"done.\n";
-
-    /*kout << "*p=" << *p << endl;
-    kout << "*q=" << *q << endl;*/
-
-    // let our stack breath!
-    //const unsigned int limit =
-        //Memory_Map<Machine>::TOP - Traits<Machine>::STACK_SIZE;
-    const unsigned int limit = 0x1ffffffc - Traits<Machine>::STACK_SIZE;
-    //Getting the memory position right after the end of the MMU Table
-    CPU::Phy_Addr base = 0x104000+0x404000-0x80000;
-    if(base % 0x4000 != 0)
-        base = base + (4 - base % 4);
-
-    db<Init, ARMv7_MMU>(TRC) << "MMU base = " << base << " limit = " << limit << "\n";
-    ARMv7_MMU::free(base, pages(limit - base));
-    db<Init, ARMv7_MMU>(TRC) << "Memory freed!" << endl;
-}
-
-void ARMv7_MMU::disable_domains()
-{
-    unsigned int x=0xffffffff; //all ones = manager (no domain check).
-    ASM("mcr p15,0,%0,c3,c0,0 \n"
-            ::"r"(x):);
-}
-
-void ARMv7_MMU::disable_access_flags()
-{
-    unsigned int old, flag = 1<<29;
-    ASM("mrc p15,0,%0,c1,c0,0 \n"
-         "bic %1, %0, %1 \n"
-         "mcr p15,0,%1,c1,c0,0 \n"
-         :"=r"(old), "=r"(flag) : : );
-}
-
-void ARMv7_MMU::disable()
-{
-    unsigned int flag;
-    ASM("mrc p15, 0, %0, c1, c0, 0 \n"
-         "bic %0, %0, #1\n"
-         "mcr p15, 0, %0, c1, c0, 0 \n":
-         "=r"(flag) : : "cc");
-}
-
-void ARMv7_MMU::set_ttbr()
-{
-    ASM("mcr p15, 0, %0, c2, c0, 0 \n"
-         "mcr p15, 0, %0, c2, c0, 1 \n"
-         : : "r"(MMU_TABLE_ADDR) : "cc");
-}
-
-unsigned int ARMv7_MMU::check_ttbr()
-{
-    unsigned int ret;
-    ASM("mrc p15,0,%0,c2,c0,2 \n"
-            :"=r"(ret) : :);
-    if((ret&0x7)==0) kout<<"Using TTBR0 only!\n";
-
-    ASM("mrc p15, 0, %0, c2, c0, 0 \n"
-         : "=r"(ret): :);
-    return ret;
-}
-
-void ARMv7_MMU::enable()
-{
-    ARMv7_MMU::set_ttbr();
-    ARMv7_MMU::disable_domains();
-    ARMv7_MMU::disable_access_flags();
-    unsigned int flag;
-
-    ASM("mrc p15, 0, %0, c1, c0, 0 \n"
-         :"=r"(flag) : :);
-    flag=(flag|1);
-    //flag=(flag|0xc03807);
-    ASM(//"orr %0, %0, #0x1 \n"
-         "dsb \n"
-         "isb \n"
-         "mcr p15, 0, %0, c1, c0, 0 \n"
-         ::"r"(flag): "cc");
-
-    ASM(
-         "dsb \n"
-         "and r0, r0, r0 \n"
-         "and r0, r0, r0 \n"
-         "dsb \n"
-         "and r0, r0, r0 \n"
-         "and r0, r0, r0 \n"
-         "isb \n"
-        );
-}
-
-void ARMv7_MMU::enable_caches()
-{   //setting bits 2 (data cache) and 12 (I-cache).
-    unsigned int flag;//, val=0x1004;
-    ASM("mrc p15, 0, %0, c1, c0, 0 \n"
-         :"=r"(flag) : : );
-    flag=flag|0x1004;
-    ASM(//"orr %0, %0, %1 \n"
-         "mcr p15, 0, %0, c1, c0, 0 \n"
-
-         "and r0, r0, r0\n"
-         "and r0, r0, r0\n"
-         "dsb\n"
-         "and r0, r0, r0\n"
-         "and r0, r0, r0\n"
-         "isb\n"
-         ::"r"(flag) : );
+    ARMv7_MMU::free(&_end, Memory_Map<Machine>::SYS_STACK - reinterpret_cast<unsigned int>(&_end));
+    //TODO: free this initial stack at Thread::init()
 }
 
 __END_SYS
+
