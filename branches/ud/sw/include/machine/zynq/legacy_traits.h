a18445ec5cf1e1b68c2cfabccdee582e417a71d3
jgreis <jgreis@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2015-03-12 13:23:43 +0000

    Zynq's Memory_Map now starts at 0x00000000 as the vector table must be at the memory bottom.
    git-svn-id: https://epos.lisha.ufsc.br/svn/epos2@3443 86035c5f-d46c-4136-a2d5-fe63f1d4a782

--- branches/ud/sw/include/machine/zynq/legacy_traits.h
+++ branches/ud/sw/include/machine/zynq/legacy_traits.h
@@ -16,14 +16,15 @@ template <> struct Traits<Zynq>: public Traits<Zynq_Common>
     static const unsigned int CPUS = Traits<Build>::CPUS;
 
     // Physical Memory
-    // Using only DDR memory, OCM doesn't support exclusive accesses needed for
-    // atomic operations
-    static const unsigned int MEM_BASE  = 0x00100000;
+    // Using only DDR memory for data, OCM doesn't support exclusive accesses
+    // needed for atomic operations. The vector table must be placed at
+    // 0x00000000.
+    static const unsigned int MEM_BASE  = 0x00000000;
     static const unsigned int MEM_TOP   = 0x080FFFFF; // 512 MB
 
     // Logical Memory Map
-    static const unsigned int APP_LOW   = 0x00100000;
-    static const unsigned int APP_CODE  = 0x00100000;
+    static const unsigned int APP_LOW   = 0x00000000;
+    static const unsigned int APP_CODE  = 0x00000000;
     static const unsigned int APP_DATA  = 0x03100000; // 192 MB
     static const unsigned int APP_HIGH  = 0x06100000; // 384 MB
 
@@ -44,14 +45,15 @@ template <> struct Traits<Zynq>: public Traits<Zynq_Common>
 
 template <> struct Traits<Zynq_IC>: public Traits<Zynq_Common>
 {
-    static const bool enabled = true;
 };
 
 template <> struct Traits<Zynq_Timer>: public Traits<Zynq_Common>
 {
-    static const bool enabled = true;
-    static const bool prescale = true;
+    static const bool debugged = hysterically_debugged;
 
+    // Meaningful values for the timer frequency range from 100 to 10000 Hz. The
+    // choice must respect the scheduler time-slice, i. e., it must be higher
+    // than the scheduler invocation frequency.
     static const int FREQUENCY = 1000; // Hz
 };
 
