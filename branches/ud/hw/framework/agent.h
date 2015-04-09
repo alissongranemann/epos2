0c35e83f6d177f737981ba0b2d3b2b94f4f860b3
jgreis <jgreis@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2015-04-09 13:38:19 +0000

    Just style.
    git-svn-id: https://epos.lisha.ufsc.br/svn/epos2@3483 86035c5f-d46c-4136-a2d5-fe63f1d4a782

--- branches/ud/hw/framework/agent.h
+++ branches/ud/hw/framework/agent.h
@@ -1,20 +1,18 @@
 #ifndef __agent_hw_h
 #define __agent_hw_h
 
+#include "catapult.h"
 #include "../../unified/framework/meta.h"
 #include "../../unified/framework/agent.h"
-#include "catapult.h"
-#include "../../unified/framework/resource_table.h"
-#include "../../unified/framework/resource_table_iids.h"
 
 namespace Implementation {
 
-template<class T>
-class Agent_Common<T, Configurations::EPOS_SOC_Catapult, true>:
-        public Serializer<Traits<T>::serdes_buffer>
+template<class Component>
+class Agent_Common<Component, Configurations::EPOS_SOC_Catapult, true>:
+        public Serializer<Traits<Component>::serdes_buffer>
 {
 public:
-    Agent_Common(Channel_t &rx_ch, Channel_t &tx_ch, unsigned int inst_id):
+    Agent_Common(Channel_t & rx_ch, Channel_t & tx_ch, unsigned int inst_id):
             Base(), _call_ch(rx_ch), _return_ch(tx_ch), _inst_id(inst_id),
             _last_call_X(0xFF), _last_call_Y(0xFF), _last_call_local(0xFF) {}
 
@@ -22,7 +20,7 @@ public:
     bool read_args() {
         typename Base::pkt_type * buff = Base::get_pkt_buffer();
 
-        for (int i = 0; i < N_ARGS; ++i) {
+        for (int i = 0; i < N_ARGS; i++) {
             _call_ch.read(_msg);
             buff[i] = _msg.phy_data.payload;
         }
@@ -40,7 +38,7 @@ public:
         _msg.phy_addr.Y = _last_call_Y;
         _msg.phy_addr.local = _last_call_local;
 
-        _msg.phy_data.header.type_id = Type2Id<T>::ID;
+        _msg.phy_data.header.type_id = Type2Id<Component>::ID;
         _msg.phy_data.header.instance_id = _inst_id;
         //FIXME RESP initiation msgs are not really needed
         //ret_msg.header.msg_type = MSG_TYPE_RESP;
@@ -48,15 +46,15 @@ public:
         //return_ch.write(ret_msg);
         _msg.phy_data.header.msg_type = MSG_TYPE_RESP_DATA;
 
-        typename Base::pkt_type* buff = Base::get_pkt_buffer();
+        typename Base::pkt_type * buff = Base::get_pkt_buffer();
 
-        for (int i = 0; i < N_RET; ++i) {
+        for (int i = 0; i < N_RET; i++) {
             _msg.phy_data.payload = buff[i];
             _return_ch.write(_msg);
         }
     }
 
-    void top_level() { static_cast<Agent<T>*>(this)->dispatch(read_header()); }
+    void top_level() { static_cast<Agent<Component>*>(this)->dispatch(read_header()); }
 
 protected:
     unsigned int _inst_id;
@@ -73,10 +71,10 @@ private:
     }
 
 private:
-    typedef Serializer<Traits<T>::serdes_buffer> Base;
+    typedef Serializer<Traits<Component>::serdes_buffer> Base;
 
-    Channel_t &_call_ch;
-    Channel_t &_return_ch;
+    Channel_t & _call_ch;
+    Channel_t & _return_ch;
 
     unsigned char _last_call_X;
     unsigned char _last_call_Y;
@@ -85,12 +83,12 @@ private:
     Catapult::RMI_Msg _msg;
 };
 
-template<typename T>
+template<typename Component>
 class Agent_Dummy
 {
 public:
-    Agent_Dummy(Channel_t &_rx_ch, Channel_t &_tx_ch, unsigned int inst_id):
-            _rx_ch(_rx_ch), _tx_ch(_tx_ch) {}
+    Agent_Dummy(Channel_t & rx_ch, Channel_t & tx_ch, unsigned int inst_id):
+            _rx_ch(rx_ch), _tx_ch(tx_ch) {}
 
     void top_level() {
         _rx_ch.read(_msg);
@@ -98,19 +96,19 @@ public:
     }
 
 private:
-    Channel_t &_rx_ch;
-    Channel_t &_tx_ch;
+    Channel_t & _rx_ch;
+    Channel_t & _tx_ch;
 
     Catapult::RMI_Msg _msg;
 };
 
 };
 
-#define DECLARE_HLS_TOP_LEVEL(name)\
-void HW_NODE(name) (Implementation::Channel_t &rx_ch, Implementation::Channel_t &tx_ch, unsigned int inst_id) {\
-    static Implementation::IF<Implementation::Traits<Implementation::name>::hardware,\
-                              Implementation::Agent<Implementation::name>,\
-                              Implementation::Agent_Dummy<Implementation::name> >::Result\
+#define HLS_TOP_LEVEL(Component)\
+void Component (Implementation::Channel_t &rx_ch, Implementation::Channel_t &tx_ch, unsigned int inst_id) {\
+    static Implementation::IF<Implementation::Traits<Implementation::Component>::hardware,\
+                              Implementation::Agent<Implementation::Component>,\
+                              Implementation::Agent_Dummy<Implementation::Component> >::Result\
         agent(rx_ch, tx_ch, inst_id);\
 \
     agent.top_level();\
