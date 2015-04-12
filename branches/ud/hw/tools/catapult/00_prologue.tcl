895f4441e6b3a79b6d559d03efe31a12755b6a32
jgreis <jgreis@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2015-04-12 21:12:33 +0000

    Add unified folder to Catapult's header file search path.
    git-svn-id: https://epos.lisha.ufsc.br/svn/epos2@3488 86035c5f-d46c-4136-a2d5-fe63f1d4a782

--- branches/ud/hw/tools/catapult/00_prologue.tcl
+++ branches/ud/hw/tools/catapult/00_prologue.tcl
@@ -3,5 +3,4 @@ project new
 options set Output/OutputVHDL true
 options set Output/OutputVerilog true
 options set Output/OutputSystemC true
-#options set Input/SearchPath ../../src
-
+options set Input/SearchPath ../../../../unified/
