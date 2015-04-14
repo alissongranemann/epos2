f40c79efe044a714cb68b9e6a25df8047e2fbb65
jgreis <jgreis@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2015-04-14 22:08:53 +0000

    Just style.
    git-svn-id: https://epos.lisha.ufsc.br/svn/epos2@3511 86035c5f-d46c-4136-a2d5-fe63f1d4a782

--- branches/ud/hw/framework/serializer.h
+++ branches/ud/hw/framework/serializer.h
@@ -56,33 +56,32 @@ protected:
     typedef unsigned long pkt;
 
 public:
-    Serializer(): _begin(&_buf[0]), _data_cnt(0) {}
+    Serializer(): _begin(&_buf[0]), _pkt_cnt(0) {}
 
     pkt * get_pkt_buf() { return &_buf[0]; }
 
-    int get_pkt_cnt() { return _data_cnt; }
-    void set_pkt_cnt(int d) { _data_cnt = d; }
-
-    void reset() { _begin = &_buf[0]; }
+    void set_pkt_cnt(int cnt) { _pkt_cnt = cnt; }
 
     template<typename T0>
     void serialize(T0 &a0) {
         Serialization<pkt>::serialize(_begin, a0);
         _begin = &_begin[data_to_pkt<T0>::Result];
-        _data_cnt += data_to_pkt<T0>::Result;
+        _pkt_cnt += data_to_pkt<T0>::Result;
     }
 
     template<typename T0>
     void deserialize(T0 &a0) {
         Serialization<pkt>::deserialize(_begin, a0);
         _begin = &_begin[data_to_pkt<T0>::Result];
-        _data_cnt -= data_to_pkt<T0>::Result;
+        _pkt_cnt -= data_to_pkt<T0>::Result;
     }
+
+    void reset() { _begin = &_buf[0]; }
 
 private:
     pkt * _begin;
-    unsigned int _data_cnt;
     pkt _buf[DIV_ROUNDUP<BUF_SIZE, sizeof(pkt)>::Result];
+    unsigned int _pkt_cnt;
 };
 
 #endif
--- branches/ud/unified/system/traits.h
+++ branches/ud/unified/system/traits.h
@@ -14,9 +14,9 @@ struct Traits
 template<> struct Traits<Build>
 {
 #ifdef HIGH_LEVEL_SYNTHESIS
-    const static bool hardware_domain = true;
+    static const bool hardware_domain = true;
 #else
-    const static bool hardware_domain = false;
+    static const bool hardware_domain = false;
 #endif
 };
 
@@ -25,14 +25,14 @@ template<> struct Traits<Adder>: public Traits<void>
     static const bool hardware = true;
 
     static const bool singleton = false;
-    const static bool static_alloc = false;
+    static const bool static_alloc = false;
+
+    static const unsigned int Alloc_Max = 0;
+    static const unsigned int serdes_buffer = 8;
+    static const unsigned int n_ids = 1;
 
     typedef void Alloc_Obj_Type;
     typedef void Alloc_Idx;
-
-    const static unsigned int Alloc_Max = 0;
-    const static unsigned int serdes_buffer = 8;
-    static const unsigned int n_ids = 1;
 };
 
 __END_SYS
