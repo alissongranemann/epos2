e001a4bb88f85293df6355b2300bfcf92fc9a8ad
davir <davir@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2015-07-29 22:34:06 +0000

    - Including support for sleep timer interrupts
    git-svn-id: https://epos.lisha.ufsc.br/svn/epos2@3677 86035c5f-d46c-4136-a2d5-fe63f1d4a782

--- branches/tstp_mac/src/machine/cortex_m/emote3_init.cc
+++ branches/tstp_mac/src/machine/cortex_m/emote3_init.cc
@@ -8,9 +8,6 @@
 
 __BEGIN_SYS
 
-// eMote3 TSC static values
-CC2538_TSC::Time_Stamp CC2538_TSC::_offset = 0;
-bool CC2538_TSC::_positive_offset;
 bool eMote3::_init_clock_done = false;
 
 void eMote3::init()
@@ -31,7 +28,7 @@ void eMote3::init_clock()
         return;
 
     // Clock setup
-    Reg32 clock_val;    
+    Reg32 clock_val;
     switch(Traits<CPU>::CLOCK)
     {
         case 32000000: clock_val = 0; break;
@@ -44,22 +41,38 @@ void eMote3::init_clock()
         case   250000: clock_val = 7; break;
         default: while(1) assert(false);
     }
-    // Enable AMP detect to make sure XOSC starts correctly
-    scr(CLOCK_CTRL) |= AMP_DET;
+
+    // Set pins PD6 and PD7 to enable external oscillator
+    {
+        const auto pin_bit = 1 << 6;
+        gpiod(AFSEL) &= ~pin_bit; // Set pin D6 as software-controlled
+        gpiod(DIR) &= ~pin_bit; // Set pin D6 as output
+        ioc(PD6_OVER) = ANA;
+    }
+    {
+        const auto pin_bit = 1 << 7;
+        gpiod(AFSEL) &= ~pin_bit; // Set pin D7 as software-controlled
+        gpiod(DIR) &= ~pin_bit; // Set pin D7 as output
+        ioc(PD7_OVER) = ANA;
+    }
 
     Reg32 clock_ctrl = scr(CLOCK_CTRL) & ~(SYS_DIV * 7);
-    clock_ctrl &= ~OSC; // Select 32Mhz oscillator
-
-    clock_ctrl &= ~OSC32K; // Select 32Khz crystal oscillator
     clock_ctrl |= clock_val * SYS_DIV; // Set system clock rate
+    clock_ctrl |= AMP_DET; // Enable AMP detect to make sure XOSC starts correctly    
+    clock_ctrl |= OSC_PD; // Power down unused oscillator
+    clock_ctrl &= ~OSC; // Select 32Mhz oscillator
+    clock_ctrl &= ~OSC32K; // Select 32Khz crystal oscillator
 
-    scr(CLOCK_CTRL) = clock_ctrl;
+    scr(CLOCK_CTRL) = clock_ctrl; // Write back to register
 
-    // Wait until oscillator stabilizes
-    while((scr(CLOCK_STA) & (STA_OSC)));
+    // Wait until oscillators stabilize
+    while((scr(CLOCK_STA) & (STA_OSC | STA_OSC32K)));
 
     clock_ctrl = scr(CLOCK_CTRL) & ~(IO_DIV * 7);
-    scr(CLOCK_CTRL) |= clock_val * IO_DIV; // Set IO clock rate
+    scr(CLOCK_CTRL) = clock_ctrl | (clock_val * IO_DIV); // Set IO clock rate
+
+    //while((scr(CLOCK_STA) & (STA_SYNC_32K))); // Contiki includes this line too for some reason 
+    while(!(scr(CLOCK_STA) & (STA_SYNC_32K)));
 
     _init_clock_done = true;
 }
---
+++ branches/tstp_mac/src/machine/cortex_m/emote3_tsc.cc
@@ -0,0 +1,26 @@
+#include <system/config.h>
+#include __MODEL_H
+#ifdef __emote3_h
+
+#include <ic.h>
+
+__USING_SYS
+
+CC2538_TSC::Time_Stamp CC2538_TSC::_offset = 0;
+
+void CC2538_TSC::wake_up_at(const CC2538_TSC::Time_Stamp & t, const CC2538_TSC::Interrupt_Handler & handler)
+{    
+    IC::disable(SM_TIMER_IRQ);
+    IC::int_vector(IC::irq2int(SM_TIMER_IRQ), handler);
+
+    while(!(reg(SMWDTHROSC_STLOAD) & STLOAD));
+
+    reg(SMWDTHROSC_ST3) = t >> 24;
+    reg(SMWDTHROSC_ST2) = t >> 16;
+    reg(SMWDTHROSC_ST1) = t >> 8;
+    reg(SMWDTHROSC_ST0) = t; // ST0 must be written last
+
+    IC::enable(SM_TIMER_IRQ);
+}
+
+#endif
