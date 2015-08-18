b3e1c37b4ae70f30abf8d93dd7124031a63e1e2f
jgreis <jgreis@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2015-08-18 19:25:27 +0000

    Add save state method to Adder.
    git-svn-id: https://epos.lisha.ufsc.br/svn/epos2@3693 86035c5f-d46c-4136-a2d5-fe63f1d4a782

--- branches/ud/unified/components/adder.h
+++ branches/ud/unified/components/adder.h
@@ -7,12 +7,33 @@ __BEGIN_SYS
 
 class Adder
 {
+private:
+    enum {
+        EXPANDED_SIZE = 176
+    };
+
 public:
     Adder() {}
 
     ~Adder() {}
 
     int add(int a, int b) { return (a + b); }
+
+    void rest_st(unsigned int * buf, unsigned int len) {
+        for(int i = 0; i < len; i++)
+            _key[i] = *(buf++);
+    }
+
+    unsigned int save_st() {
+        static unsigned int i = 0;
+
+        return _key[i++];
+    }
+
+    unsigned int get_st_len() { return EXPANDED_SIZE; }
+
+private:
+    unsigned int _key[EXPANDED_SIZE];
 };
 
 __END_SYS
