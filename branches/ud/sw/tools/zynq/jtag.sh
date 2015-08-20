6d448bc665fab2748046a488eec4cd51d425c8e9
jgreis <jgreis@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2015-08-20 14:45:32 +0000

    Add GDB debug support for applications executed on Zynq.
    git-svn-id: https://epos.lisha.ufsc.br/svn/epos2@3698 86035c5f-d46c-4136-a2d5-fe63f1d4a782

--- branches/ud/sw/tools/zynq/jtag.sh
+++ branches/ud/sw/tools/zynq/jtag.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-BASE_SCRIPT="$DIR"/xmd_script.tcl
-
-if [ "$#" -ne 1 ]; then
-	echo -e "ERROR!\nUsage: $0 <application.hex>"
-	exit 1
-fi
+BASE_INI="$DIR"/base_xmd.ini
+INI="$DIR"/xmd.ini
+TMP="$DIR"/tmp
 
 APP=$1
+GDB=$2
 
-if [ ! -f $BASE_SCRIPT ]; then
-	echo -e "ERROR!\nFile $BASE_SCRIPT not found"
-	exit 2
+# Check if xmd is on $PATH
+hash xmd > /dev/null 2>/dev/null
+
+if [ $? -ne 0 ] 
+then
+    echo xmd not found in '$PATH'
+    read -n1 -r -p "Press any key to exit"
+    exit 1
 fi
-if [ ! -f $APP ]; then
-	echo -e "ERROR!\nFile $APP not found"
-	exit 3
+
+# Running xmd -tcl will execute the commands and exit immediately closing the
+# connection with GDB that's why we rely on the xmd.ini file. xmd.ini doesn't
+# support arguments so we use sed to insert the arguments in it.
+sed 's:APP:'"$APP"':g' $BASE_INI > $TMP
+
+# Check if $GDB is set
+if [ -e $GDB 1 ]
+    then sed 's:GDB:'1':g' $TMP > $INI
+else 
+    sed 's:GDB:'0':g' $TMP > $INI
 fi
 
-SCRIPT="$BASE_SCRIPT"_$RANDOM
+cd $DIR
+xmd
 
-sed 's:APPLICATION_NAME:'"$APP"':g' $BASE_SCRIPT > $SCRIPT
-
-xmd -tcl $SCRIPT 
-
-rm $SCRIPT
+rm $INI $TMP
