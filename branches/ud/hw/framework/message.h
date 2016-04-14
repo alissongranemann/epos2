9c083ebd653aedc9346fc5243bfe1068cbea371a
jgreis <jgreis@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2016-04-14 01:53:41 +0000

    Implement pack() and unpack() methods in Message to handle data from the interface generated by Vivado HLS. This is a workaround: Vivado HLS doesn't seem to able to pack elements from structs used as arguments in a top level function.
    git-svn-id: https://epos.lisha.ufsc.br/svn/epos2@3909 86035c5f-d46c-4136-a2d5-fe63f1d4a782

--- branches/ud/hw/framework/message.h
+++ branches/ud/hw/framework/message.h
@@ -1,7 +1,9 @@
 #ifndef __message_h
 #define __message_h
 
-#include <ac_channel.h>
+#include <hls_stream.h>
+#include <ap_int.h>
+
 #include <system/config.h>
 
 #include "../../unified/rtsnoc.h"
@@ -48,20 +50,23 @@ public:
     typedef int Method;
     typedef Method Result;
 
-    typedef ac_channel<Packet> Channel;
+    typedef ap_uint<80> u80;
+    typedef hls::stream<u80> Channel;
 
 public:
     Message(Channel & rx_ch, Channel & tx_ch): _rx_ch(rx_ch), _tx_ch(tx_ch) {}
 
     unsigned int method() {
-        _rx_ch.read(_msg);
+        _rx_ch.read(_u);
+        pack(_u, _msg);
         return _msg.payload;
     }
 
     template<typename T0, typename T1>
     void in2(T0 & a0, T1 & a1) {
         for (unsigned int i = 0; i < Serializer::NPKT2<T0, T1>::Result; i++) {
-            _rx_ch.read(_msg);
+            _rx_ch.read(_u);
+            pack(_u, _msg);
             _parms[i] = _msg.payload;
         }
 
@@ -79,14 +84,37 @@ public:
 
         for (unsigned int i = 0; i < Serializer::NPKT1<T0>::Result; i++) {
             _msg.payload = _parms[i];
-            _tx_ch.write(_msg);
+            unpack(_u, _msg);
+            _tx_ch.write(_u);
         }
+    }
+
+private:
+    void pack(u80 &u, Packet &msg) {
+        msg.payload = u.range(31, 0);
+        msg.header.type = u.range(39, 32);
+        msg.header.inst_id = u.range(47, 40);
+        msg.header.type_id = u.range(55, 48);
+        msg.addr._x = u.range(63, 56);
+        msg.addr._y = u.range(71, 64);
+        msg.addr._local = u.range(79, 72);
+    }
+
+    void unpack(u80 &u, Packet &msg) {
+        u(31, 0) = msg.payload;
+        u(39, 32) = msg.header.type;
+        u(47, 40) = msg.header.inst_id;
+        u(55, 48) = msg.header.type_id;
+        u(63, 56) = msg.addr._x;
+        u(71, 64) = msg.addr._y;
+        u(79, 72) = msg.addr._local;
     }
 
 private:
     Channel & _rx_ch;
     Channel & _tx_ch;
     Packet _msg;
+    u80 _u;
     Buffer _parms[MAX_PARAMETERS_SIZE];
 };
 
