0da588b1910dfb58f41fcebca601cd5789134f76
jgreis <jgreis@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2016-06-25 21:18:12 +0000

    Simplify Zynq_UART register names
    git-svn-id: https://epos.lisha.ufsc.br/svn/epos2@4045 86035c5f-d46c-4136-a2d5-fe63f1d4a782

--- branches/ud/sw/include/machine/cortex_a/zynq.h
+++ branches/ud/sw/include/machine/cortex_a/zynq.h
@@ -33,50 +33,35 @@ private:
         RCVR_FIFO_TRIGGER_LEVEL0    = 0x20,
         CHANNEL_STS_REG0            = 0x2C,
         TX_RX_FIFO0                 = 0x30,
-        BAUD_RATE_DIVIDER_REG0      = 0x34,
+        BAUD_RATE_DIVIDER_REG0      = 0x34
     };
 
     // Useful bits in the CONTROL_REG0 register
     enum {
-        CONTROL_REG0_RXRES  = 0,
-        CONTROL_REG0_TXRES  = 1,
-        CONTROL_REG0_RXEN   = 2,
-        CONTROL_REG0_RXDIS  = 3,
-        CONTROL_REG0_TXEN   = 4,
-        CONTROL_REG0_TXDIS  = 5,
-        CONTROL_REG0_RSTTO  = 6,
-        CONTROL_REG0_STTBRK = 7,
-        CONTROL_REG0_STPBRK = 8
+        RXRES   = 0,
+        TXRES   = 1,
+        RXEN    = 2,
+        TXEN    = 4
     };
 
     // Useful bits in the MODE_REG0 register
     enum {
-        MODE_REG0_CLKS      = 0,
-        MODE_REG0_CHRL      = 1,
-        MODE_REG0_PAR       = 3,
-        MODE_REG0_NBSTOP    = 6,
-        MODE_REG0_CHMODE    = 8
+        CHRL    = 1,
+        PAR     = 3,
+        NBSTOP  = 6,
+        CHMODE  = 8
     };
 
-    // Useful bits in the INTRPT_EN_REG0, and INTRPT_DIS_REG0
-    // registers
+    // Useful bits in the INTRPT_EN_REG0, and INTRPT_DIS_REG0 registers
     enum {
-        INTRPT_REG0_RTRIG = 0,
-        INTRPT_REG0_TTRIG = 10,
+        INTRPT_RTRIG = 0,
+        INTRPT_TTRIG = 10
     };
 
     // Useful bits in the CHANNEL_STS_REG0 register
     enum {
-        CHANNEL_STS_REG0_RTRIG      = 0,
-        CHANNEL_STS_REG0_REMPTY     = 1,
-        CHANNEL_STS_REG0_RFUL       = 2,
-        CHANNEL_STS_REG0_TEMPTY     = 3,
-        CHANNEL_STS_REG0_TFUL       = 4,
-        CHANNEL_STS_REG0_RACTIVE    = 10,
-        CHANNEL_STS_REG0_TACTIVE    = 11,
-        CHANNEL_STS_REG0_FDELT      = 12,
-        CHANNEL_STS_REG0_TTRIG      = 13,
-        CHANNEL_STS_REG0_TNFUL      = 14
+        STS_RTRIG   = 0,
+        STS_TFUL    = 4
     };
 
 public:
@@ -94,11 +79,11 @@ public:
         reg(BAUD_RATE_GEN_REG0, 0x3E);
 
         // Enable and reset RX/TX data paths
-        reg(CONTROL_REG0, reg(CONTROL_REG0) | 1<<CONTROL_REG0_RXRES | 1<<CONTROL_REG0_TXRES | 1<<CONTROL_REG0_RXEN |
-            1<<CONTROL_REG0_TXEN);
+        reg(CONTROL_REG0, reg(CONTROL_REG0) | 1<<RXRES | 1<<TXRES | 1<<RXEN |
+            1<<TXEN);
 
         // Set 1 stop bit, no parity and 8 data bits
-        reg(MODE_REG0, reg(MODE_REG0) | 0x0<<MODE_REG0_CHRL | 0x4<<MODE_REG0_PAR | 0x0<<MODE_REG0_NBSTOP);
+        reg(MODE_REG0, reg(MODE_REG0) | 0x0<<CHRL | 0x4<<PAR | 0x0<<NBSTOP);
 
         // Set the receiver trigger level to 1
         reg(RCVR_FIFO_TRIGGER_LEVEL0, 1);
@@ -108,27 +93,27 @@ public:
     void txd(unsigned char ch) { reg(TX_RX_FIFO0, (Reg32)ch); }
 
     void int_enable(bool receive = true, bool send = true, bool line = true, bool modem = true) {
-        reg(INTRPT_EN_REG0, reg(INTRPT_EN_REG0) | (receive << INTRPT_REG0_RTRIG) | (send << INTRPT_REG0_TTRIG));
-        reg(INTRPT_DIS_REG0, reg(INTRPT_DIS_REG0) & ~(receive << INTRPT_REG0_RTRIG) & ~(send << INTRPT_REG0_TTRIG));
+        reg(INTRPT_EN_REG0, reg(INTRPT_EN_REG0) | (receive << INTRPT_RTRIG) | (send << INTRPT_TTRIG));
+        reg(INTRPT_DIS_REG0, reg(INTRPT_DIS_REG0) & ~(receive << INTRPT_RTRIG) & ~(send << INTRPT_TTRIG));
     }
     void int_disable(bool receive = true, bool send = true, bool line = true, bool modem = true) {
-        reg(INTRPT_EN_REG0, reg(INTRPT_EN_REG0) & ~(receive << INTRPT_REG0_RTRIG) & ~(send << INTRPT_REG0_TTRIG));
-        reg(INTRPT_DIS_REG0, reg(INTRPT_DIS_REG0) | (receive << INTRPT_REG0_RTRIG) | (send << INTRPT_REG0_TTRIG));
+        reg(INTRPT_EN_REG0, reg(INTRPT_EN_REG0) & ~(receive << INTRPT_RTRIG) & ~(send << INTRPT_TTRIG));
+        reg(INTRPT_DIS_REG0, reg(INTRPT_DIS_REG0) | (receive << INTRPT_RTRIG) | (send << INTRPT_TTRIG));
     }
 
     void loopback(bool flag) {
         Reg32 mode = reg(MODE_REG0) & ~0x300;
 
         if(flag)
-            mode |= 2 << MODE_REG0_CHMODE;
+            mode |= 2 << CHMODE;
         else
-            mode |= 0 << MODE_REG0_CHMODE;
+            mode |= 0 << CHMODE;
 
         reg(MODE_REG0, mode);
     }
 
-    bool rxd_ok() { return reg(CHANNEL_STS_REG0) & (1 << CHANNEL_STS_REG0_RTRIG); }
-    bool txd_ok() { return !(reg(CHANNEL_STS_REG0) & (1<<CHANNEL_STS_REG0_TFUL)); }
+    bool rxd_ok() { return reg(CHANNEL_STS_REG0) & (1 << STS_RTRIG); }
+    bool txd_ok() { return !(reg(CHANNEL_STS_REG0) & (1 << STS_TFUL)); }
 
 private:
     typedef CPU::Reg32 Reg32;
