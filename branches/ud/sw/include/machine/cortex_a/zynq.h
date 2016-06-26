8937c55497354e372a6a6bdca5224f9d262dbd0e
jgreis <jgreis@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2016-06-26 15:55:05 +0000

    Refactor Zynq_UART's useful bits.
    git-svn-id: https://epos.lisha.ufsc.br/svn/epos2@4053 86035c5f-d46c-4136-a2d5-fe63f1d4a782

--- branches/ud/sw/include/machine/cortex_a/zynq.h
+++ branches/ud/sw/include/machine/cortex_a/zynq.h
@@ -31,50 +31,50 @@ private:
 
 private:
     // Register addresses relative to base
-    enum {
-        CONTROL_REG0                = 0x00,
-        MODE_REG0                   = 0x04,
-        INTRPT_EN_REG0              = 0x08,
-        INTRPT_DIS_REG0             = 0x0C,
-        BAUD_RATE_GEN_REG0          = 0x18,
-        RCVR_FIFO_TRIGGER_LEVEL0    = 0x20,
-        CHANNEL_STS_REG0            = 0x2C,
-        TX_RX_FIFO0                 = 0x30,
-        BAUD_RATE_DIVIDER_REG0      = 0x34
+    enum {                                      // Description              Type    Value after reset
+        CONTROL_REG0                = 0x00,     // Control                  r/w     0x00000128
+        MODE_REG0                   = 0x04,     // Mode                     r/w     0x00000000
+        INTRPT_EN_REG0              = 0x08,     // Interrupt enable         r/w     0x00000000
+        INTRPT_DIS_REG0             = 0x0C,     // Interrupt disable        r/w     0x00000000
+        BAUD_RATE_GEN_REG0          = 0x18,     // Baud rate generator      r/w     0x0000028B
+        RCVR_FIFO_TRIGGER_LEVEL0    = 0x20,     // Receive trigger level    r/w     0x00000020
+        CHANNEL_STS_REG0            = 0x2C,     // Channel status           ro      0x00000000
+        TX_RX_FIFO0                 = 0x30,     // Transmit/receive FIFO    r/w     0x00000000
+        BAUD_RATE_DIVIDER_REG0      = 0x34      // Baud rate divider        r/w     0x0000000F
     };
 
     // Useful bits in the CONTROL_REG0 register
-    enum {
-        RXRES   = 0,
-        TXRES   = 1,
-        RXEN    = 2,
-        TXEN    = 4
+    enum {                                      // Description              Type    Value after reset
+        RXRES                       = 1 << 0,   // Reset Rx data path       r/w     0
+        TXRES                       = 1 << 1,   // Reset Tx data path       r/w     0
+        RXEN                        = 1 << 2,   // Receive enable           r/w     0
+        TXEN                        = 1 << 4    // Transmit enable          r/w     0
     };
 
     // Useful bits in the MODE_REG0 register
-    enum {                      // Description              Type    Value after reset
-        CHRL8       = 0 << 1,   // Character Length 8 bits  r/w     0
-        CHRL7       = 2 << 1,   // Character Length 7 bits  r/w     0
-        CHRL6       = 3 << 1,   // Character Length 6 bits  r/w     0
-        PAREVEN     = 0 << 3,   // Even parity              r/w     0
-        PARODD      = 1 << 3,   // Odd parity               r/w     0
-        PARNONE     = 4 << 3,   // No parity                r/w     0
-        NBSTOP2     = 2 << 6,   // 2 stop bits              r/w     0
-        NBSTOP1     = 0 << 6,   // 1 stop bit               r/w     0
-        CHMODENORM  = 0 << 8,   // Normal mode              r/w     0
-        CHMODELB    = 2 << 8    // Loopback mode            r/w     0
+    enum {                                      // Description              Type    Value after reset
+        CHRL8                       = 0 << 1,   // Character Length 8 bits  r/w     0
+        CHRL7                       = 2 << 1,   // Character Length 7 bits  r/w     0
+        CHRL6                       = 3 << 1,   // Character Length 6 bits  r/w     0
+        PAREVEN                     = 0 << 3,   // Even parity              r/w     0
+        PARODD                      = 1 << 3,   // Odd parity               r/w     0
+        PARNONE                     = 4 << 3,   // No parity                r/w     0
+        NBSTOP2                     = 2 << 6,   // 2 stop bits              r/w     0
+        NBSTOP1                     = 0 << 6,   // 1 stop bit               r/w     0
+        CHMODENORM                  = 0 << 8,   // Normal mode              r/w     0
+        CHMODELB                    = 2 << 8    // Loopback mode            r/w     0
     };
 
     // Useful bits in the INTRPT_EN_REG0, and INTRPT_DIS_REG0 registers
-    enum {
-        INTRPT_RTRIG = 0,
-        INTRPT_TTRIG = 10
+    enum {                                      // Description              Type    Value after reset
+        INTRPT_RTRIG                = 1 << 0,   // Receiver FIFO empty      wo      0
+        INTRPT_TTRIG                = 1 << 10   // Transmitter FIFO trigger wo      0
     };
 
     // Useful bits in the CHANNEL_STS_REG0 register
-    enum {
-        STS_RTRIG   = 0,
-        STS_TFUL    = 4
+    enum {                                      // Description              Type    Value after reset
+        STS_RTRIG                   = 1 << 0,   // Receiver FIFO trigger    ro      0
+        STS_TFUL                    = 1 << 4    // Transmitter FIFO full    ro      0
     };
 
 public:
@@ -102,19 +102,19 @@ public:
         reg(RCVR_FIFO_TRIGGER_LEVEL0) = 1;
 
         // Enable and reset RX and TX data paths
-        reg(CONTROL_REG0) |= (1 << RXRES) | (1 << TXRES) | (1 << RXEN) | (1 << TXEN);
+        reg(CONTROL_REG0) |= RXRES | TXRES | RXEN | TXEN;
     }
 
     unsigned char rxd() { return reg(TX_RX_FIFO0); }
     void txd(unsigned char ch) { reg(TX_RX_FIFO0) = ch; }
 
     void int_enable(bool receive = true, bool send = true, bool line = true, bool modem = true) {
-        reg(INTRPT_EN_REG0) |= (receive << INTRPT_RTRIG) | (send << INTRPT_TTRIG);
-        reg(INTRPT_DIS_REG0) &= ~(receive << INTRPT_RTRIG) & ~(send << INTRPT_TTRIG);
+        reg(INTRPT_EN_REG0) |= (receive ? INTRPT_RTRIG : 0) | (send ? INTRPT_TTRIG : 0);
+        reg(INTRPT_DIS_REG0) &= ~(receive ? INTRPT_RTRIG : 0) & ~(send ? INTRPT_TTRIG : 0);
     }
     void int_disable(bool receive = true, bool send = true, bool line = true, bool modem = true) {
-        reg(INTRPT_EN_REG0) &= ~(receive << INTRPT_RTRIG) & ~(send << INTRPT_TTRIG);
-        reg(INTRPT_DIS_REG0) |= (receive << INTRPT_RTRIG) | (send << INTRPT_TTRIG);
+        reg(INTRPT_EN_REG0) &= ~(receive ? INTRPT_RTRIG : 0) & ~(send ? INTRPT_TTRIG : 0);
+        reg(INTRPT_DIS_REG0) |= (receive ? INTRPT_RTRIG : 0) | (send ? INTRPT_TTRIG : 0);
     }
 
     void loopback(bool flag) {
@@ -128,8 +128,8 @@ public:
         reg(MODE_REG0) = mode;
     }
 
-    bool rxd_ok() { return reg(CHANNEL_STS_REG0) & (1 << STS_RTRIG); }
-    bool txd_ok() { return !(reg(CHANNEL_STS_REG0) & (1 << STS_TFUL)); }
+    bool rxd_ok() { return reg(CHANNEL_STS_REG0) & STS_RTRIG; }
+    bool txd_ok() { return !(reg(CHANNEL_STS_REG0) & STS_TFUL); }
 
 private:
     volatile Reg32 & reg(unsigned int o) { return reinterpret_cast<volatile Reg32*>(_base)[o / sizeof(Reg32)]; }
