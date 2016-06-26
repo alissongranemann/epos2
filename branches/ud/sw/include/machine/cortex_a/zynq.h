0a747835e3d42dccd19a76f54ecf88c0b8470998
jgreis <jgreis@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2016-06-26 18:23:08 +0000

    Just style.
    git-svn-id: https://epos.lisha.ufsc.br/svn/epos2@4064 86035c5f-d46c-4136-a2d5-fe63f1d4a782

--- branches/ud/sw/include/machine/cortex_a/zynq.h
+++ branches/ud/sw/include/machine/cortex_a/zynq.h
@@ -16,85 +16,132 @@ protected:
 public:
     // Base address for memory-mapped System Control Registers
     enum {
-        UART0_BASE          = 0xE0000000,
-        UART1_BASE          = 0xE0001000,
-        SLCR_BASE           = 0xF8000000,
-        CPU_ITF_BASE        = 0xF8F00100,
-        GLOBAL_TIMER_BASE   = 0xF8F00200,
-        PRIV_TIMER_BASE     = 0XF8F00600,
-        DIST_BASE           = 0xF8F01000
+        UART0_BASE                  = 0xE0000000,
+        UART1_BASE                  = 0xE0001000,
+        SLCR_BASE                   = 0xF8000000,
+        CPU_ITF_BASE                = 0xF8F00100,
+        GLOBAL_TIMER_BASE           = 0xF8F00200,
+        PRIV_TIMER_BASE             = 0XF8F00600,
+        DIST_BASE                   = 0xF8F01000
+    };
+
+    // Register addresses relative to base
+    enum {                                      // Description              Type    Value after reset
+        CONTROL_REG0                = 0x00,     // Control                  r/w     0x00000128
+        MODE_REG0                   = 0x04,     // Mode                     r/w     0x00000000
+        INTRPT_EN_REG0              = 0x08,     // Interrupt enable         r/w     0x00000000
+        INTRPT_DIS_REG0             = 0x0C,     // Interrupt disable        r/w     0x00000000
+        BAUD_RATE_GEN_REG0          = 0x18,     // Baud rate generator      r/w     0x0000028B
+        RCVR_FIFO_TRIGGER_LEVEL0    = 0x20,     // Receive trigger level    r/w     0x00000020
+        CHANNEL_STS_REG0            = 0x2C,     // Channel status           ro      0x00000000
+        TX_RX_FIFO0                 = 0x30,     // Transmit/receive FIFO    r/w     0x00000000
+        BAUD_RATE_DIVIDER_REG0      = 0x34      // Baud rate divider        r/w     0x0000000F
+    };
+
+    // Useful bits in the CONTROL_REG0 register
+    enum {                                      // Description              Type    Value after reset
+        RXRES                       = 1 << 0,   // Reset Rx data path       r/w     0
+        TXRES                       = 1 << 1,   // Reset Tx data path       r/w     0
+        RXEN                        = 1 << 2,   // Receive enable           r/w     0
+        TXEN                        = 1 << 4    // Transmit enable          r/w     0
+    };
+
+    // Useful bits in the MODE_REG0 register
+    enum {                                      // Description              Type    Value after reset
+        CHRL8                       = 0 << 1,   // Character Length 8 bits  r/w     0
+        CHRL7                       = 2 << 1,   // Character Length 7 bits  r/w     0
+        CHRL6                       = 3 << 1,   // Character Length 6 bits  r/w     0
+        PAREVEN                     = 0 << 3,   // Even parity              r/w     0
+        PARODD                      = 1 << 3,   // Odd parity               r/w     0
+        PARNONE                     = 4 << 3,   // No parity                r/w     0
+        NBSTOP2                     = 2 << 6,   // 2 stop bits              r/w     0
+        NBSTOP1                     = 0 << 6,   // 1 stop bit               r/w     0
+        CHMODENORM                  = 0 << 8,   // Normal mode              r/w     0
+        CHMODELB                    = 2 << 8    // Loopback mode            r/w     0
+    };
+
+    // Useful bits in the INTRPT_EN_REG0, and INTRPT_DIS_REG0 registers
+    enum {                                      // Description              Type    Value after reset
+        INTRPT_RTRIG                = 1 << 0,   // Receiver FIFO empty      wo      0
+        INTRPT_TTRIG                = 1 << 10   // Transmitter FIFO trigger wo      0
+    };
+
+    // Useful bits in the CHANNEL_STS_REG0 register
+    enum {                                      // Description              Type    Value after reset
+        STS_RTRIG                   = 1 << 0,   // Receiver FIFO trigger    ro      0
+        STS_TFUL                    = 1 << 4    // Transmitter FIFO full    ro      0
     };
 
     // SLCR Registers offsets
-    enum {                              // Description
-        FPGA0_CLK_CTRL  = 0x170,        // PL Clock 0 Output control
-        PSS_RST_CTRL    = 0x200,        // PS Software Reset Control
-        FPGA_RST_CTRL   = 0x240         // FPGA Software Reset Control
+    enum {                                      // Description
+        FPGA0_CLK_CTRL              = 0x170,    // PL Clock 0 Output control
+        PSS_RST_CTRL                = 0x200,    // PS Software Reset Control
+        FPGA_RST_CTRL               = 0x240     // FPGA Software Reset Control
     };
 
     // Useful bits in FPGAN_CLK_CTRL
-    enum {                              // Description                  Type    Value after reset
-        DIVISOR0        = 1 << 8,       // First cascade divider        r/w     0x18
-        DIVISOR1        = 1 << 20       // Second cascade divider       r/w     0x1
+    enum {                                      // Description                  Type    Value after reset
+        DIVISOR0                    = 1 << 8,   // First cascade divider        r/w     0x18
+        DIVISOR1                    = 1 << 20   // Second cascade divider       r/w     0x1
     };
 
     // CPU Interface Registers offsets
-    enum {                              // Description
-        ICCICR          = 0x000,        // CPU Interface Control
-        ICCPMR          = 0x004,        // Interrupt Priority Mask
-        ICCIAR          = 0x00C,        // Interrupt Ack
-        ICCEOIR         = 0x010         // End Of Interrupt
+    enum {                                      // Description
+        ICCICR                      = 0x000,    // CPU Interface Control
+        ICCPMR                      = 0x004,    // Interrupt Priority Mask
+        ICCIAR                      = 0x00C,    // Interrupt Ack
+        ICCEOIR                     = 0x010     // End Of Interrupt
     };
 
     // Useful bits in ICCICR
-    enum {                              // Description                  Type    Value after reset
-        ITF_EN_S        = 1 << 0,       // Enable secure signaling      r/w     0
-        ITF_EN_NS       = 1 << 1,       // Enable non-secure signaling  r/w     0
-        ACK_CTL         = 1 << 2        // Acknowledge control          r/w     0
+    enum {                                      // Description                  Type    Value after reset
+        ITF_EN_S                    = 1 << 0,   // Enable secure signaling      r/w     0
+        ITF_EN_NS                   = 1 << 1,   // Enable non-secure signaling  r/w     0
+        ACK_CTL                     = 1 << 2    // Acknowledge control          r/w     0
     };
 
     // Global Timer Registers offsets
-    enum {                              // Description
-        GTCTRL          = 0x00,         // Low Counter
-        GTCTRH          = 0x04,         // High Counter
-        GTCLR           = 0x08,         // Control
-        GTISR           = 0x0C          // Interrupt Status
+    enum {                                      // Description
+        GTCTRL                      = 0x00,     // Low Counter
+        GTCTRH                      = 0x04,     // High Counter
+        GTCLR                       = 0x08,     // Control
+        GTISR                       = 0x0C      // Interrupt Status
     };
 
     // Private Timer Registers offsets
-    enum {                              // Description
-        PTLR            = 0x00,         // Load
-        PTCTR           = 0x04,         // Counter
-        PTCLR           = 0x08,         // Control
-        PTISR           = 0x0C          // Interrupt Status
+    enum {                                      // Description
+        PTLR                        = 0x00,     // Load
+        PTCTR                       = 0x04,     // Counter
+        PTCLR                       = 0x08,     // Control
+        PTISR                       = 0x0C      // Interrupt Status
     };
 
     // Useful bits in PTCLR
-    enum {                              // Description                  Type    Value after reset
-        TIMER_ENABLE    = 1 << 0,       // Enable                       r/w     0
-        AUTO_RELOAD     = 1 << 1,       // Auto reload                  r/w     0
-        IRQ_EN          = 1 << 2        // Enable interrupt             r/w     0
+    enum {                                      // Description                  Type    Value after reset
+        TIMER_ENABLE                = 1 << 0,   // Enable                       r/w     0
+        AUTO_RELOAD                 = 1 << 1,   // Auto reload                  r/w     0
+        IRQ_EN                      = 1 << 2    // Enable interrupt             r/w     0
     };
 
     // Useful bits in PTISR
-    enum {                              // Description                  Type    Value after reset
-        INT_CLR         = 1 << 0        // Interrupt clear bit          r/w     0
+    enum {                                      // Description                  Type    Value after reset
+        INT_CLR                     = 1 << 0    // Interrupt clear bit          r/w     0
     };
 
     // Distributor Registers offsets
-    enum {                              // Description
-        ICDDCR          = 0x000,        // Distributor Control
-        ICDISER0        = 0x100,        // Interrupt Set-Enable
-        ICDISER1        = 0x104,        // Interrupt Set-Enable
-        ICDISER2        = 0x108,        // Interrupt Set-Enable
-        ICDICER0        = 0x180,        // Interrupt Clear-Enable
-        ICDICER1        = 0x184,        // Interrupt Clear-Enable
-        ICDICER2        = 0x188         // Interrupt Clear-Enable
+    enum {                                      // Description
+        ICDDCR                      = 0x000,    // Distributor Control
+        ICDISER0                    = 0x100,    // Interrupt Set-Enable
+        ICDISER1                    = 0x104,    // Interrupt Set-Enable
+        ICDISER2                    = 0x108,    // Interrupt Set-Enable
+        ICDICER0                    = 0x180,    // Interrupt Clear-Enable
+        ICDICER1                    = 0x184,    // Interrupt Clear-Enable
+        ICDICER2                    = 0x188     // Interrupt Clear-Enable
     };
 
     // Useful bits in ICDDCR
-    enum {                              // Description                  Type    Value after reset
-        DIST_EN_S       = 1 << 0        // Enable secure interrupts     r/w     0
+    enum {                                      // Description                  Type    Value after reset
+        DIST_EN_S                   = 1 << 0    // Enable secure interrupts     r/w     0
     };
 
 protected:
@@ -167,59 +214,8 @@ typedef Zynq Cortex_A_Model;
 class Zynq_UART: protected Zynq
 {
 private:
-    typedef CPU::Log_Addr Log_Addr;
-    typedef CPU::Reg32 Reg32;
-
     static const unsigned int UNITS = Traits<UART>::UNITS;
     static const unsigned int CLOCK = Traits<UART>::CLOCK;
-
-private:
-    // Register addresses relative to base
-    enum {                                      // Description              Type    Value after reset
-        CONTROL_REG0                = 0x00,     // Control                  r/w     0x00000128
-        MODE_REG0                   = 0x04,     // Mode                     r/w     0x00000000
-        INTRPT_EN_REG0              = 0x08,     // Interrupt enable         r/w     0x00000000
-        INTRPT_DIS_REG0             = 0x0C,     // Interrupt disable        r/w     0x00000000
-        BAUD_RATE_GEN_REG0          = 0x18,     // Baud rate generator      r/w     0x0000028B
-        RCVR_FIFO_TRIGGER_LEVEL0    = 0x20,     // Receive trigger level    r/w     0x00000020
-        CHANNEL_STS_REG0            = 0x2C,     // Channel status           ro      0x00000000
-        TX_RX_FIFO0                 = 0x30,     // Transmit/receive FIFO    r/w     0x00000000
-        BAUD_RATE_DIVIDER_REG0      = 0x34      // Baud rate divider        r/w     0x0000000F
-    };
-
-    // Useful bits in the CONTROL_REG0 register
-    enum {                                      // Description              Type    Value after reset
-        RXRES                       = 1 << 0,   // Reset Rx data path       r/w     0
-        TXRES                       = 1 << 1,   // Reset Tx data path       r/w     0
-        RXEN                        = 1 << 2,   // Receive enable           r/w     0
-        TXEN                        = 1 << 4    // Transmit enable          r/w     0
-    };
-
-    // Useful bits in the MODE_REG0 register
-    enum {                                      // Description              Type    Value after reset
-        CHRL8                       = 0 << 1,   // Character Length 8 bits  r/w     0
-        CHRL7                       = 2 << 1,   // Character Length 7 bits  r/w     0
-        CHRL6                       = 3 << 1,   // Character Length 6 bits  r/w     0
-        PAREVEN                     = 0 << 3,   // Even parity              r/w     0
-        PARODD                      = 1 << 3,   // Odd parity               r/w     0
-        PARNONE                     = 4 << 3,   // No parity                r/w     0
-        NBSTOP2                     = 2 << 6,   // 2 stop bits              r/w     0
-        NBSTOP1                     = 0 << 6,   // 1 stop bit               r/w     0
-        CHMODENORM                  = 0 << 8,   // Normal mode              r/w     0
-        CHMODELB                    = 2 << 8    // Loopback mode            r/w     0
-    };
-
-    // Useful bits in the INTRPT_EN_REG0, and INTRPT_DIS_REG0 registers
-    enum {                                      // Description              Type    Value after reset
-        INTRPT_RTRIG                = 1 << 0,   // Receiver FIFO empty      wo      0
-        INTRPT_TTRIG                = 1 << 10   // Transmitter FIFO trigger wo      0
-    };
-
-    // Useful bits in the CHANNEL_STS_REG0 register
-    enum {                                      // Description              Type    Value after reset
-        STS_RTRIG                   = 1 << 0,   // Receiver FIFO trigger    ro      0
-        STS_TFUL                    = 1 << 4    // Transmitter FIFO full    ro      0
-    };
 
 public:
     Zynq_UART(unsigned int unit, unsigned int baud_rate, unsigned int data_bits, unsigned int parity, unsigned int stop_bits)
