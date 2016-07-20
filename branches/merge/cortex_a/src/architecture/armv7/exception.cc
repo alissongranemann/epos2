c538f1a31cd2beb5947a9055c715a9c5fd0ae3c4
jgreis <jgreis@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2016-07-20 19:46:14 +0000

    Lock on while when exception occurs.
    git-svn-id: https://epos.lisha.ufsc.br/svn/epos2@4106 86035c5f-d46c-4136-a2d5-fe63f1d4a782

--- branches/merge/cortex_a/src/architecture/armv7/exception.cc
+++ branches/merge/cortex_a/src/architecture/armv7/exception.cc
@@ -8,42 +8,42 @@ void _undefined_instruction() __attribute__ ((naked));
 void _undefined_instruction()
 {
     kout << "undefined instruction\n";
-    ASM("movs pc, r14");
+    while(1);
 }
 
 void _software_interrupt() __attribute__ ((naked));
 void _software_interrupt()
 {
     kout << "software interrupt\n";
-    ASM("movs pc, r14");
+    while(1);
 }
 
 void _prefetch_abort() __attribute__ ((naked));
 void _prefetch_abort()
 {
     kout << "prefetch abort\n";
-    ASM("subs pc, r14, #4");
+    while(1);
 }
 
 void _data_abort() __attribute__ ((naked));
 void _data_abort()
 {
     kout << "data abort\n";
-    ASM("subs pc, r14, #8");
+    while(1);
 }
 
 void _reserved() __attribute__ ((naked));
 void _reserved()
 {
     kout << "reserved\n";
-    ASM("mov pc, r14");
+    while(1);
 }
 
 void _fiq() __attribute__ ((naked));
 void _fiq()
 {
     kout << "fiq handler\n";
-    ASM("subs pc, r14, #4");
+    while(1);
 }
 
 };
