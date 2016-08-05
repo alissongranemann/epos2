f5c8404fa0c7c3b92efb6777f659f0adcfa09a2c
jgreis <jgreis@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2016-08-05 15:07:40 +0000

    Enable INT_TIMER for Zynq on Cortex_Timer::init().
    git-svn-id: https://epos.lisha.ufsc.br/svn/epos2@4136 86035c5f-d46c-4136-a2d5-fe63f1d4a782

--- branches/merge/cortex_a/src/machine/cortex/timer_init.cc
+++ branches/merge/cortex_a/src/machine/cortex/timer_init.cc
@@ -11,6 +11,11 @@ void Cortex_Timer::init()
 
     Engine::init(FREQUENCY);
     IC::int_vector(IC::INT_TIMER, int_handler);
+    // TODO: Maybe the ifdef isn't necessary. Where is INT_TIMER enabled on
+    // Cortex-M?
+#ifdef __mmod_zynq__
+    IC::enable(IC::INT_TIMER);
+#endif
     Engine::enable();
 }
 
