8e722612528ce9cc1a93b85c893e5c6224065dfd
mateus <mateus@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2016-09-22 21:53:04 +0000

    Copied epos2/trunk revision 4207 for testing and features integration.
    git-svn-id: https://epos.lisha.ufsc.br/svn/epos2@4208 86035c5f-d46c-4136-a2d5-fe63f1d4a782

---
+++ branches/merge/ukernel/include/architecture/armv7/traits.h
@@ -0,0 +1,32 @@
+// EPOS ARMv7 Architecture Metainfo
+#ifndef __armv7_traits_h
+#define __armv7_traits_h
+
+#include <system/config.h>
+
+__BEGIN_SYS
+
+template <> struct Traits<CPU>: public Traits<void>
+{
+    enum {LITTLE, BIG};
+    static const unsigned int ENDIANESS         = LITTLE;
+    static const unsigned int WORD_SIZE         = 32;
+    static const unsigned int CLOCK             = Traits<Build>::MODEL == Traits<Build>::LM3S811 ? 50000000 : Traits<Build>::MODEL == Traits<Build>::Zynq ? 666666687 : 32000000;
+    static const bool unaligned_memory_access   = false;
+};
+
+template <> struct Traits<MMU>: public Traits<void>
+{
+    static const bool colorful = false;
+    static const unsigned int COLORS = 1;
+};
+
+template <> struct Traits<TSC>: public Traits<void>
+{
+    static const bool enabled = (Traits<Build>::MACHINE == Traits<Build>::Cortex_A);
+//TODO: http://stackoverflow.com/questions/16236460/arm-cortex-a9-event-counters-return-0
+};
+
+__END_SYS
+
+#endif
