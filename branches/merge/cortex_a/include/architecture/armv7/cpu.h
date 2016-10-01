fc541b8f76d350cfc7d015d50d9f7f4d271e5d63
davir <davir@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2016-10-01 00:19:32 +0000

    Restoring armv7 CPU
    git-svn-id: https://epos.lisha.ufsc.br/svn/epos2@4263 86035c5f-d46c-4136-a2d5-fe63f1d4a782

--- branches/merge/cortex_a/include/architecture/armv7/cpu.h
+++ branches/merge/cortex_a/include/architecture/armv7/cpu.h
@@ -61,7 +61,7 @@ public:
         return disabled;
     }
 
-    static void mrs12() { ASM("mrs r12, xpsr" : : : "r12" ); }
+    static void mrs12() { ASM("mrs r12, xpsr" : : : "r12"); }
     static void msr12() { ASM("msr xpsr, r12" : : : "cc"); }
 };
 
@@ -251,7 +251,7 @@ public:
         ASM("mov r0, %0" : : "r"(fr) : "r0");
     }
 
-    static Log_Addr ip() {// due to RISC pipelining PC is read with a +8 (4 for thumb) offset
+    static Log_Addr ip() { // due to RISC pipelining PC is read with a +8 (4 for thumb) offset
         Reg32 value;
         ASM("mov %0, pc" : "=r"(value) :);
         return value;
@@ -264,8 +264,8 @@ public:
     static T tsl(volatile T & lock) {
         register T old;
         register T one = 1;
-        ASM("1: ldrex   %0, [%1]        \n"
-            "   strex   r3, %2, [%1]    \n"
+        ASM("1: ldrexb  %0, [%1]        \n"
+            "   strexb  r3, %2, [%1]    \n"
             "   cmp     r3, #0          \n"
             "   bne     1b              \n" : "=&r" (old) : "r"(&lock), "r"(one) : "r3");
         return old;
@@ -274,23 +274,49 @@ public:
     template <typename T>
     static T finc(volatile T & value) {
         register T old;
-        ASM("1: ldrex   %0, [%1]        \n"
-            "   add     %0, #1          \n"
-            "   strex   r3, %0, [%1]    \n"
-            "   cmp     r3, #0          \n"
-            "   bne     1b              \n" : "=&r" (old) : "r"(&value) : "r3" );
-        return old;
+        if(sizeof(T) == sizeof(Reg8))
+            ASM("1: ldrexb  %0, [%1]        \n"
+                "   add     %0, #1          \n"
+                "   strexb  r3, %0, [%1]    \n"
+                "   cmp     r3, #0          \n"
+                "   bne     1b              \n" : "=&r" (old) : "r"(&value) : "r3");
+        else if(sizeof(T) == sizeof(Reg16))
+            ASM("1: ldrexh  %0, [%1]        \n"
+                "   add     %0, #1          \n"
+                "   strexh  r3, %0, [%1]    \n"
+                "   cmp     r3, #0          \n"
+                "   bne     1b              \n" : "=&r" (old) : "r"(&value) : "r3");
+        else
+            ASM("1: ldrex   %0, [%1]        \n"
+                "   add     %0, #1          \n"
+                "   strex   r3, %0, [%1]    \n"
+                "   cmp     r3, #0          \n"
+                "   bne     1b              \n" : "=&r" (old) : "r"(&value) : "r3");
+        return old - 1;
     }
 
     template <typename T>
     static T fdec(volatile T & value) {
         register T old;
-        ASM("1: ldrex   %0, [%1]        \n"
-            "   sub     %0, #1          \n"
-            "   strex   r3, %0, [%1]    \n"
-            "   cmp     r3, #0          \n"
-            "   bne     1b              \n" : "=&r" (old) : "r"(&value) : "r3" );
-        return old;
+        if(sizeof(T) == sizeof(Reg8))
+            ASM("1: ldrexb  %0, [%1]        \n"
+                "   sub     %0, #1          \n"
+                "   strexb  r3, %0, [%1]    \n"
+                "   cmp     r3, #0          \n"
+                "   bne     1b              \n" : "=&r" (old) : "r"(&value) : "r3");
+        else if(sizeof(T) == sizeof(Reg16))
+            ASM("1: ldrexh  %0, [%1]        \n"
+                "   sub     %0, #1          \n"
+                "   strexh  r3, %0, [%1]    \n"
+                "   cmp     r3, #0          \n"
+                "   bne     1b              \n" : "=&r" (old) : "r"(&value) : "r3");
+        else
+            ASM("1: ldrex   %0, [%1]        \n"
+                "   sub     %0, #1          \n"
+                "   strex   r3, %0, [%1]    \n"
+                "   cmp     r3, #0          \n"
+                "   bne     1b              \n" : "=&r" (old) : "r"(&value) : "r3");
+        return old + 1;
     }
 
     template <typename T>
