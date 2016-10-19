d00abca22f567e3569aa1527e8e7dac4aa8756f7
davir <davir@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2016-10-19 20:23:17 +0000

    Fixed some TSTP_MAC parameters
    git-svn-id: https://epos.lisha.ufsc.br/svn/epos2@4327 86035c5f-d46c-4136-a2d5-fe63f1d4a782

--- branches/arm/src/machine/cortex/cc2538_test_traits.h
+++ branches/arm/src/machine/cortex/cc2538_test_traits.h
@@ -121,7 +121,7 @@ template<> struct Traits<System>: public Traits<void>
 
     enum {FOREVER = 0, SECOND = 1, MINUTE = 60, HOUR = 3600, DAY = 86400, WEEK = 604800, MONTH = 2592000, YEAR = 31536000};
     static const unsigned long long LIFE_SPAN = 1 * HOUR; // in seconds
-    static const unsigned int DUTY_CYCLE = 1000; // in ppm
+    static const unsigned int DUTY_CYCLE = 10000; // in ppm
 
     static const bool reboot = true;
 
