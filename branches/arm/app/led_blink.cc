440dab1dd3df5780825ce1f3d1fb8bbe4e3c39da
davir <davir@86035c5f-d46c-4136-a2d5-fe63f1d4a782>
2017-02-02 15:45:02 +0000

    Updating led_blink app
    git-svn-id: https://epos.lisha.ufsc.br/svn/epos2@4438 86035c5f-d46c-4136-a2d5-fe63f1d4a782

--- branches/arm/app/led_blink.cc
+++ branches/arm/app/led_blink.cc
@@ -4,11 +4,12 @@ using namespace EPOS;
 
 int main()
 {
-    GPIO g('c',3, GPIO::OUTPUT);
-    for(bool b=false;;b=(b+1)%2)
-    {
+    GPIO g('C',3, GPIO::OUT);
+
+    for(bool b=false;;b=(b+1)%2) {
         g.set(b);
         for(volatile int t=0;t<0xfffff;t++);
     }
+
     return 0;
 }
