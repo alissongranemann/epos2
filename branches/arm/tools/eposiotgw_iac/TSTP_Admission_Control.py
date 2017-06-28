5f899637f2836041549ba4c755d8f9ef9c3986bf
alissongranemann <alissongranemannabreu@gmail.com>
2017-06-28 01:07:41 -0300

    correções e melhorias

--- branches/arm/tools/eposiotgw_iac/TSTP_Admission_Control.py
+++ branches/arm/tools/eposiotgw_iac/TSTP_Admission_Control.py
@@ -1,7 +1,7 @@
 from Serial_Manager import Serial_Manager
 from Burden_Estimator import Burden_Estimator
 from TSTP_WSN import TSTP_WSN
-import struct
+from TSTP_Exception import *
 
 class TSTP_Admission_Control:
 
@@ -9,17 +9,19 @@ class TSTP_Admission_Control:
         self.wsn = TSTP_WSN()
         self.estimator = Burden_Estimator(self.wsn)
         self.test_mode = test_mode
-        if(not self.test_mode):
+        if(not test_mode):
             self.serial_manager = Serial_Manager(self)
 
     def config(self, sink_radius, mac_period):
         self.estimator.set_mac_period(mac_period)
+        self.wsn.set_gateway_range(sink_radius)
 
     def handle_new_sensor_request(self, sensor):
         isAceptable = self.estimator.is_sensor_aceptable(sensor)
         if(isAceptable):
             self.wsn.add_sensor(sensor)
         #self.notify(isAceptable)
+        self.estimator.update_interests_without_response()
 
     def handle_new_interest_request(self, interest):
         isAceptable = False#self.estimator.is_interest_aceptable(interest)
--- branches/arm/tools/eposiotgw_iac/TSTP_WSN.py
+++ branches/arm/tools/eposiotgw_iac/TSTP_WSN.py
@@ -1,16 +1,18 @@
 from Sensor_Rtree import Sensor_Rtree
 from Sensor_Graph import Sensor_Graph
 from Sensor import Sensor
+from TSTP_Exception import NoSensorInRegionException
 
 class TSTP_WSN:
 
-    rtree = Sensor_Rtree()
-    graph = Sensor_Graph()
-    gateway = Sensor(0, 0, 10)
+    def __init__(self):
+        self.rtree = Sensor_Rtree()
+        self.graph = Sensor_Graph()
+        self.gateway = Sensor(0, 0, 10)
+        self.add_sensor(self.gateway)
 
     def add_sensor(self, sensor):
         if(self.graph.contains(sensor)):
-            print("Sensor already inserted on TSTP")
             return
 
         self.rtree.add_sensor(sensor)
@@ -23,10 +25,13 @@ class TSTP_WSN:
         self.graph.add_edges(sensor, reachable_sensors)
 
     def add_interest(self, interest):
-        print ("interest added on TSTP")
+        pass
 
     def get_sensors_in_range(self, coordinate_object):
-        return self.rtree.get_sensors_in_range(coordinate_object)
+        sensors_in_range = self.rtree.get_sensors_in_range(coordinate_object)
+        if(len(sensors_in_range) == 0):
+            raise NoSensorInRegionException
+        return sensors_in_range
 
     def get_nearest_sensor(self, sensors_in_region):
         return self.rtree.get_nearest_sensor(self.gateway, sensors_in_region)
@@ -36,3 +41,6 @@ class TSTP_WSN:
 
     def draw(self):
         self.graph.draw()
+
+    def set_gateway_range(self, range):
+        self.gateway.range = range;
