713bef1ed5120c0f77963310f0addb69b5520485
Alisson <alissongranemannabreu@gmail.com>
2017-06-29 14:21:08 -0300

    correção iac test

--- branches/arm/app/iac_tstp_gateway.cc
+++ branches/arm/app/iac_tstp_gateway.cc
@@ -9,19 +9,13 @@ OStream cout;
 typedef TSTP::Coordinates Coordinates;
 typedef TSTP::Region Region;
 
-const unsigned int PERIOD = 145;
+const unsigned int MAC_PERIOD = 145;
 const unsigned int INTEREST_PERIOD = 1000000;
 const unsigned int INTEREST_EXPIRY = 2 * INTEREST_PERIOD;
 
 int main()
 {
-    cout << "TSTP Gateway test" << endl;
-
-    cout << "My machine ID is:";
-    for(unsigned int i = 0; i < 8; i++)
-        cout << " " << hex << Machine::id()[i];
-    cout << endl;
-    cout << "You can set this value at src/component/tstp_init.cc to set initial coordinates for this mote." << endl;
+    cout << "TSTP IAC Gateway test" << endl;
 
     cout << "My coordinates are " << TSTP::here() << endl;
     cout << "The time now is " << TSTP::now() << endl;
@@ -30,18 +24,9 @@ int main()
     Coordinates center_sensor(10,10,0);
     Region region_sensor(center_sensor, 0, TSTP::now(), -1);
 
-    cout << "I will now ask for Acceleration data from any sensor located in " << region_sensor << endl;
-
-    Acceleration acceleration3(region_sensor, INTEREST_EXPIRY, INTEREST_PERIOD);
-    Acceleration acceleration1(region_sensor, INTEREST_EXPIRY, 145 * 2);
-    Acceleration acceleration2(region_sensor, INTEREST_EXPIRY, 145 * 2);
-
-    while(true) {
-        Alarm::delay(INTEREST_PERIOD);
-        cout << "Acceleration in " << acceleration1.location() << " at " << acceleration1.time() << " was " << acceleration1 << endl;
-        cout << "Acceleration in " << acceleration2.location() << " at " << acceleration2.time() << " was " << acceleration2 << endl;
-        cout << "Acceleration in " << acceleration3.location() << " at " << acceleration3.time() << " was " << acceleration3 << endl;
-    }
+    Acceleration acceleration1(region_sensor, INTEREST_EXPIRY, INTEREST_PERIOD); //1% burden
+    Acceleration acceleration2(region_sensor, INTEREST_EXPIRY, MAC_PERIOD * 2); //50% burden
+    Acceleration acceleration3(region_sensor, INTEREST_EXPIRY, MAC_PERIOD * 2); //50% burden
 
     return 0;
 }
--- branches/arm/app/iac_tstp_sensor.cc
+++ branches/arm/app/iac_tstp_sensor.cc
@@ -8,13 +8,7 @@ OStream cout;
 
 int main()
 {
-    cout << "TSTP Sensor test" << endl;
-
-    cout << "My machine ID is:";
-    for(unsigned int i = 0; i < 8; i++)
-        cout << " " << hex << Machine::id()[i];
-    cout << endl;
-    cout << "You can set this value at src/component/tstp_init.cc to set initial coordinates for this mote." << endl;
+    cout << "TSTP IAC Sensor test" << endl;
 
     cout << "My coordinates are " << TSTP::here() << endl;
     cout << "The time now is " << TSTP::now() << endl;
